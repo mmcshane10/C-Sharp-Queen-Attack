72d16b687cb319618cf842754c2afee4845da24d
Christopher Burge and Mike McShane <christopher@>
2019-09-23 14:07:30 -0700

    create and application that determines if the queen can attack another chess piece depending on coordinates.

---
+++ queen.cs
@@ -0,0 +1,38 @@
+using System;
+
+class Queen
+{
+  static void Main()
+  {
+    Console.WriteLine("What is the X coordinate of your Queen? [choose a number between 1-8]");
+    int queenXInt = int.Parse(Console.ReadLine());
+    Console.WriteLine("What is the Y coordinate of your Queen? [choose a letter between A-H]");
+    string queenYPosition = Console.ReadLine();
+
+    Console.WriteLine("What is the X coordinate of your Enemy's piece? [choose a number between 1-8]");
+    int enemyXInt = int.Parse(Console.ReadLine());
+    Console.WriteLine("What is the Y coordinate of your Enemy's piece? [choose a letter between A-H]");
+    string enemyYPosition = Console.ReadLine();
+    
+    string[] yCoord = {"A", "B", "C", "D", "E", "F", "G", "H"};
+    int queenYInt = Array.IndexOf(yCoord, queenYPosition) + 1;
+    int enemyYInt = Array.IndexOf(yCoord, enemyYPosition) + 1;
+    int slope = Math.Abs((queenYInt - enemyYInt) / (queenXInt - enemyXInt));
+
+    if (queenXInt == enemyXInt)
+    {
+      Console.WriteLine("You Can Attack!");
+    }
+    else if (queenYPosition == enemyYPosition)
+    {
+      Console.WriteLine("You Can Attack!");
+    }
+    else if (slope == 1)
+    {
+      Console.WriteLine("You Can Attack!");
+    }
+    else {
+      Console.WriteLine("You CANNOT Attack!");
+    }
+  }
+}
